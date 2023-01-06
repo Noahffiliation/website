cea23b841f182f81d9910b5e97ed3d617dba7f0a
noahffiliation <nlindsey15@gmail.com>
2023-01-06 09:45:13 -0500

    Successfully pulling my tv watchlist from trakt

--- controllers/tvController.js
+++ controllers/tvController.js
@@ -1,5 +1,19 @@
-exports.tv_list = (req, res, next) => {
+var request = require('request');
+require('dotenv').config();
 
+exports.tv_list = (req, res, next) => {
+    request({
+        method: 'GET',
+        url: 'https://api.trakt.tv/users/noahffiliation/watchlist/shows/released',
+        headers: {
+          'Content-Type': 'application/json',
+          'trakt-api-version': '2',
+          'trakt-api-key': process.env.TRAKT_API_KEY
+        }}, function (error, response, body) {
+            body = JSON.parse(body);
+            body = body.reverse();
+            res.render('tv', { title: "TV Watchlist", tv: body })
+      });
 };
 
 exports.tv_detail = (req, res, next) => {
