88071c7a755989ed1c7b27aa7a251dfd23b47334
noahffiliation <nlindsey15@gmail.com>
2023-01-19 13:02:22 -0500

    Going through Express security practices

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const session = require("cookie-session");
 const logger = require("morgan");
+const helmet = require('helmet')
 
 const indexRouter = require("./routes/index");
 
@@ -12,6 +14,18 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.set('trust proxy', 1);
+
+app.use(session({
+	name: "session",
+	secret: process.env.SESSION_SECRET,
+	cookie: {
+		secure: true,
+		httpOnly: true,
+	}
+}));
+
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,15 +33,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/favicon.ico", express.static("public/images/favicon.ico"));
 
+app.disable('x-powered-by');
+
 app.use("/", indexRouter);
 
 // Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
 	next(createError(404));
 });
 
 // Error handler
-app.use(function (err, req, res) {
+app.use((err, req, res) =>{
 	// Set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get("env") === "development" ? err : {};
