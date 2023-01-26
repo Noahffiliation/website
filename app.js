31de1de7684b90657cc2305afb1381b293a2efdd
noahffiliation <nlindsey15@gmail.com>
2023-01-26 14:02:46 -0500

    Missing semicolon, added another header to games I'm currently playing, removing letterboxd lists from the end of the rss object

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const limiter = rateLimit({
 	max: 100,
 	windowMs: 60 * 60 * 1000,
 	message: 'Too many requests from this IP, please try again in an hour!'
-})
+});
 
 app.use(helmet());
 app.use(compression());
